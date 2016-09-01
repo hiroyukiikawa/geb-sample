f4417097d268c27937d59da240e2d246f3f8aa46
hiroyuki.ikawa <hiroyuki.ikawa@bizreach.co.jp>
2016-09-01 10:24:53 +0900

    テストデータを用意する

--- src/test/groovy/stanby.groovy
+++ src/test/groovy/stanby.groovy
@@ -1,5 +1,6 @@
 import geb.spock.GebReportingSpec
 import org.openqa.selenium.Keys
+import spock.lang.Unroll
 
 class SampleTest extends GebReportingSpec {
   def "トップページを表示する"() {
@@ -23,6 +24,29 @@ class SampleTest extends GebReportingSpec {
 
     then: "検索結果画面が表示される"
     title == "ビズリーチの求人 | スタンバイ"
+  }
+
+  @Unroll("#keyword, locationで検索する")
+  def "トップページでいろいろ検索してみる"(String keyword, String location) {
+    when: "スタンバイを表示"
+    go "https://jp.stanby.com"
+
+    and: "キーワードと勤務地を入力"
+    $("#jsi-input-keyword") << keyword
+    $("#jsi-input-location") << location
+    $("#jsi-input-keyword") << Keys.ENTER
+
+    waitFor { $("h1", 0).isDisplayed() }
+
+    then: "検索結果が表示され、検索フォームに検索条件が入力されている"
+    $("#jsi-keyword").value()  == keyword
+    $("#jsi-location").value() == location
+
+    where :
+    keyword  | location
+    "飲食店" | "千葉県"
+    "営業"   | "大阪府"
+    "鹿"     | "奈良県"
   }
 
 }
